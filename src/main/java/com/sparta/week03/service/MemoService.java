f3dce56f66c361e0f885ab0b637147808dba8a59
naracodes <naralee0124@gmail.com>
2021-01-03 14:38:42 -0500

    add requiredargsconstructor, service, transactional

--- src/main/java/com/sparta/week03/service/MemoService.java
+++ src/main/java/com/sparta/week03/service/MemoService.java
@@ -4,12 +4,16 @@ import com.sparta.week03.domain.Memo;
 import com.sparta.week03.domain.MemoRepository;
 import com.sparta.week03.domain.MemoRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Service
 public class MemoService {
 
     private final MemoRepository memoRepository; // final => IS A MUST!
 
+    @Transactional
     public Long update(Long id, MemoRequestDto requestDto) {
         Memo memo = memoRepository.findById(id).orElseThrow(
                 () -> new NullPointerException("No such id exists")
